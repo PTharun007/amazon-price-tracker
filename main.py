b6e39ffceb6375fb51c30c256661ebc8e35be3a4
PTharun007 <ptharunsagar@gmail.com>
2025-05-16 09:17:57 +0530

    Add

---
+++ main.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import requests
+import smtplib
+import os
+from dotenv import load_dotenv
+load_dotenv()
+live_url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-US,en;q=0.9,te;q=0.8",
+    "Priority": "u=0, i",
+    "Sec-Ch-Ua": '"Chromium";v="136", "Google Chrome";v="136", "Not.A/Brand";v="99"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"Windows"',
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "cross-site",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
+}
+response = requests.get(live_url,headers=headers)
+soup = BeautifulSoup(response.content, "html.parser")
+price = soup.find(class_="a-offscreen").get_text()
+price_without_currency = price.split("$")[1]
+price_as_float = float(price_without_currency)
+print(price_as_float)
+"""MAIL SENDING"""
+title = soup.find(id="productTitle").get_text().strip().split(" 9")[0]
+# Set the price below which you would like to get a notification
+BUY_PRICE = 100
+if price_as_float < BUY_PRICE:
+    message = f"{title} is on sale for {price}!"
+    print(message)
+    with smtplib.SMTP(os.environ["SMTP_ADDRESS"], port=587) as connection:
+        connection.starttls()
+        result = connection.login(os.environ["EMAIL_ADDRESS"], os.environ["EMAIL_PASSWORD"])
+        connection.sendmail(
+            from_addr=os.environ["EMAIL_ADDRESS"],
+            to_addrs=os.environ["SENDING_EMAIL_ADDRESS"],
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{live_url}".encode("utf-8")
+        )
